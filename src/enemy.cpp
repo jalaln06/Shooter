0632c18a04c67f56bba4a720b210975259da191d
Jalal Naghiyev <jalal.naghiyev.06@gmail.com>
2020-04-17 11:52:01 +0300

    Added enemy class
    Fixed bounds of screen

---
+++ src/enemy.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jalal on 17.04.2020.
+//
+
+#include "enemy.h"
---
+++ src/enemy.h
@@ -0,0 +1,15 @@
+//
+// Created by Jalal on 17.04.2020.
+//
+
+#ifndef SHOOTER_ENEMY_H
+#define SHOOTER_ENEMY_H
+
+
+class enemy
+{
+
+};
+
+
+#endif //SHOOTER_ENEMY_H
